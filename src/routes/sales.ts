13ad9d46b34043199f88529af3375ed96cdd1fc1
Galder315 <315galder@gmail.com>
2023-03-10 09:11:00 +0100

    cambios rutas ofertas

--- src/routes/sales.ts
+++ src/routes/sales.ts
@@ -7,7 +7,22 @@ const router: Router = express.Router()
 router.get('/', async (req, res) => {
   try {
     const request = new mssql.Request()
-    const result = await request.query(`select * from sales`)
+    const result =
+      await request.query(`select s.*, u.name as username from sales s join users u on s.user_id = u.user_id
+    `)
+    res.json(getDBFormattedResponse(200, result.recordset)).status(200).end()
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const request = new mssql.Request()
+    request.input('id', mssql.Int, req.params.id)
+    const result = await request.query(
+      `select s.*, u.name as username from sales s join users u on s.user_id = u.user_id where s.sale_id = @id`
+    )
     res.json(getDBFormattedResponse(200, result.recordset)).status(200).end()
   } catch (err) {
     console.log(err)
