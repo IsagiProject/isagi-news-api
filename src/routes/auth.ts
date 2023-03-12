e6525d89900ef6ccddbcbbac9878ec2b8cc8944a
Galder315 <315galder@gmail.com>
2023-03-12 16:56:44 +0100

    recuperacion de contraseña POST

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -87,14 +87,4 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/recover', async (req, res) => {
-  const { email } = req.body
-
-  try {
-    sendRecoverMail(email)
-    res.send('ok').status(200).end()
-  } catch (err) {
-    console.log(err)
-  }
-})
 export default router
