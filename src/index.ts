6b8715534fda9361ca02e44e528d602db050e262
Galder315 <315galder@gmail.com>
2023-03-24 11:22:16 +0100

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
   next()
 })
 
